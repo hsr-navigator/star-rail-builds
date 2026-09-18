31b57c4a95a7b853b2e54b4016d4207268f3c60d
Val <valaerys.t@gmail.com>
2026-09-18 22:50:15 +0200

    adding de

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -4,8 +4,8 @@ export const languages = [
     { code: 'cht', name: '繁體中文' },
     { code: 'vn', name: 'Tiếng Việt' },
     { code: 'fr', name: 'Français' },
-    /*{ code: 'de', name: 'Deutsch' },
-    { code: 'es', name: 'Español' },
+    { code: 'de', name: 'Deutsch' },
+    /*{ code: 'es', name: 'Español' },
     { code: 'it', name: 'Italiano' },
     { code: 'pt', name: 'Português' },*/
 ] as const;
